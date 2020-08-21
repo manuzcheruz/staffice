f916dfd245666c0ef58e4ea82540d197d14b55c3
manuzcheruz <kipkemoi.emanuel@gmail.com>
2020-08-21 22:06:55 +0300

    done except social links

--- staffice/admin.py
+++ staffice/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Contact
 # Register your models here.
+
+
+class ContactAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Contact, ContactAdmin)
--- staffice/views.py
+++ staffice/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .forms import ContactForm
+from django.contrib import messages
 
 # Create your views here.
+
+
+def home_page(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, f'Your message has been saved')
+    else:
+        form = ContactForm()
+
+    return render(request, 'staffice/index.html', {'form': form})
