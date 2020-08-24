a89039277d12c78245f2f5da7e1862c6d3c714a8
manuz cheruz <kipkemoi.emanuel@gmail.com>
2020-08-24 15:08:51 +0300

    removed secret key

--- consult/settings.py
+++ consult/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+r=f8alkbiqw%o!k+3@6nli2a5s!y%)xzeclq!nji1c2_=^c%l'
+SECRET_KEY = '#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
